455d1dd3976c2854716b4bffc7a94f2664ef344f
hanmingk <95327699+hanmingk@users.noreply.github.com>
2024-06-17 14:09:31 +0800

    Update main.cpp

--- paddle_lite_ocr/src/main.cpp
+++ paddle_lite_ocr/src/main.cpp
@@ -29,24 +29,24 @@ int main(int, char **) {
     Flags flags = flags_default();
     delete[] flags.det_model_file;
     flags.det_model_file = toCstr(
-        "/home/hmk/Documents/ocr_model/ch_PP-OCRv4_det_infer/"
+        "~/Documents/ocr_model/ch_PP-OCRv4_det_infer/"
         "ch_PP-OCRv4_det_opt.nb");
     delete[] flags.rec_model_file;
     flags.rec_model_file = toCstr(
-        "/home/hmk/Documents/ocr_model/ch_PP-OCRv4_rec_infer/"
+        "~/Documents/ocr_model/ch_PP-OCRv4_rec_infer/"
         "ch_PP-OCRv4_rec_opt.nb");
     delete[] flags.cls_model_file;
     flags.cls_model_file = toCstr(
-        "/home/hmk/Documents/ocr_model/ch_ppocr_mobile_v2.0_cls_infer/"
+        "~/Documents/ocr_model/ch_ppocr_mobile_v2.0_cls_infer/"
         "ch_ppocr_mobile_v2.0_cls_opt.nb");
     delete[] flags.dict_path;
     flags.dict_path =
-        toCstr("/home/hmk/code/cpp/paddle_lite_ocr/asset/ppocr_keys_v1.txt");
+        toCstr("~/code/cpp/paddle_lite_ocr/asset/ppocr_keys_v1.txt");
 
     PPOCR *ppocr = init_ppocr(flags);
 
     OCRPredictResultArray array;
-    ocr(ppocr, "/home/hmk/code/cpp/paddle_lite_ocr/asset/11.jpg", array);
+    ocr(ppocr, "~/code/cpp/paddle_lite_ocr/asset/11.jpg", array);
     print_results(array);
 
     release_ppocr(ppocr);
